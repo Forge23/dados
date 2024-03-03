50cdb5337d15d9b40783b069846dcace77823d1e
Forge <angelxone@outlook.com>
2024-03-03 14:56:56 -0600

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dados';
+  dadoIzquierdo='../assets/img/d1.png';
+  dadoDerecho='../assets/img/d4.png';
+  numero1: number=0;
+  numero2: number=0;
+  
+  tirarDados(): void{
+    this.numero1=Math.round(Math.random()*5)+1;
+    this.numero2=Math.round(Math.random()*5)+1;
+    this.dadoIzquierdo='../assets/img/d'+this.numero1+'.png';
+    this.dadoDerecho='../assets/img/d'+this.numero2+'.png';
+  }
 }
